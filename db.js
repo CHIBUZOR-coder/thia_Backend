b6130f170327b1f4270372e97b3f698730bc2e01
Chibuzor <chibuzormekalam@gmail.com>
2025-03-30 07:03:21 +0100

    changed connection method

--- db.js
+++ db.js
@@ -1,3 +1,28 @@
+// import pg from "pg";
+// import dotenv from "dotenv";
+// dotenv.config();
+
+// const { Client } = pg;
+
+// const client = new Client({
+//   host: process.env.DB_HOST, // Supabase host
+//   port: process.env.DB_PORT, // Default PostgreSQL port
+//   user: process.env.DB_USERNAME, // Supabase username
+//   password: process.env.DB_PASSWORD, // Supabase password
+//   database: process.env.DB_DATABASE, // Supabase database
+//   ssl: { rejectUnauthorized: false }, // Required for Supabase connection
+// });
+
+// (async () => {
+//   try {
+//     await client.connect();
+//     console.log("Database connected successfully via Session Pooler");
+//   } catch (error) {
+//     console.error("Database connection error:", error.message);
+//   }
+// })();
+
+// export default client;
 import pg from "pg";
 import dotenv from "dotenv";
 dotenv.config();
@@ -5,12 +30,8 @@ dotenv.config();
 const { Client } = pg;
 
 const client = new Client({
-  host: process.env.DB_HOST, // Supabase host
-  port: process.env.DB_PORT, // Default PostgreSQL port
-  user: process.env.DB_USERNAME, // Supabase username
-  password: process.env.DB_PASSWORD, // Supabase password
-  database: process.env.DB_DATABASE, // Supabase database
-  ssl: { rejectUnauthorized: false }, // Required for Supabase connection
+  connectionString: process.env.DATABASE_URL,
+  ssl: false, // Disable SSL
 });
 
 (async () => {
@@ -23,3 +44,4 @@ const client = new Client({
 })();
 
 export default client;
+
