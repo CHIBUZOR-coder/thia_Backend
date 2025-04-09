73bf62c7ec06861fbd2375021ff51fb276fe0f30
Chibuzor <chibuzormekalam@gmail.com>
2025-04-09 19:50:19 +0100

    added my locahost

--- server.js
+++ server.js
@@ -13,26 +13,22 @@ import { searchRoute } from "./Routers/searchRouter.js";
 const app = express();
 import dotenv from "dotenv";
 
-
-
 dotenv.config();
-
 
 const port = process.env.PORT || 5000;
 // console.log(`Type of password: ${typeof process.env.DB_PASSWORD}`);
 //Middlewares
 app.use(
   cors({
-    origin: "https://thia-e-comerce.vercel.app", // Ensure your frontend URL is allowed
+    // origin: "https://thia-e-comerce.vercel.app", // Ensure your frontend URL is allowed
+    origin: [
+      "https://thia-e-comerce.vercel.app",
+      "http://localhost:5173/", // local dev, optional
+    ],
     methods: ["GET", "POST", "DELETE", "PUT"], // Allow specific HTTP methods
     credentials: true, // Allow cookies and other credentials
   })
 );
-
-
-
-
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
